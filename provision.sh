4d3c6b793ebbd1ff9458456227fba2934764afce
Orxan <orxan@rehimli.info>
2015-10-25 02:37:01 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,32 +1,39 @@
-#!/usr/bin/env bash
-set e+
-sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_0.10 | sudo -E bash -
-sudo add-get-repository ppa:docker-maint/testing
-sudo apt-get update
-sudo apt-get install -y git build-essential g++ nodejs
-sudo apt-get install -y docker.io
+#!/bin/bash
 
-sudo npm install -g npm
+apt-get update
+
+# Install build tools
+apt-get install -y g++ git curl vim build-essential
+
+#nodejs
+apt-get update
+apt-get install -y python-software-properties python g++ make
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
+
+# npm packages
 sudo npm install -g bower
 sudo npm install -g nscale
 
-sudo chown -R vagrant:vagrant /usr/local
-sudo chown -R vagrant:vagrant /var/run/docker.sock
+# Docker
+wget -q -O - https://get.docker.io/gpg | apt-key add -
+echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+apt-get update 
+apt-get install -y --force-yes lxc-docker
 
-curl –sSL https://test.docker.com/ubuntu | sh
 
-#enter this command for using docker
-#sudo usermod -aG docker $(whoami)
+#Set time
+sudo echo "Europe/Baku" | sudo tee /etc/timezone
+sudo dpkg-reconfigure -f noninteractive tzdata
+sudo apt-get update -y
 
-#For using nscale you must create git config file as follows:
-#git config --global user.name "orxan07"
-#git config --global user.email "orxan@rehimli.info"
-#ssh-keygen -t rsa -b 4096 -C "orxan@rehimli.info"
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/id_rsa
-#cat ~/.ssh/id_rsa.pub
-# copy and add rsa_key to user settings (SSH keys) in github.com
 
-# tidy up
-apt-get autoremove -y
+# Setup locales
+echo -e "LC_CTYPE=en_US.UTF-8\nLC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" | tee -a /etc/environment > /dev/null
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
