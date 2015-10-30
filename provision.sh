7a2e330384fdaf05dc080ef021da8af612b00047
Orxan <orxan@rehimli.info>
2015-10-30 14:06:02 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 # Install build tools
-sudo apt-get install -y g++ git curl vim build-essential
+sudo apt-get install -y g++ git curl lxc wget bsdtar vim build-essential 
 
 #nodejs
 sudo apt-get update
@@ -12,15 +12,21 @@ sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
 
-# npm packages
-sudo npm install -g nscale
+
 
 # Docker
 wget -q -O - https://get.docker.io/gpg | apt-key add -
 echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 sudo apt-get update 
 sudo apt-get install -y --force-yes lxc-docker
+sudo apt-get install -y docker.io
 
+sudo chown -R vagrant:vagrant /usr/local
+sudo chown -R vagrant:vagrant /var/run/docker.sock
+
+# npm packages
+sudo npm install -g npm
+sudo npm install -g nscale
 
 #Set time
 sudo echo "Europe/Moscow" | sudo tee /etc/timezone
@@ -30,9 +36,11 @@ sudo apt-get update -y
 
 # Setup locales
 echo -e "LC_CTYPE=en_US.UTF-8\nLC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" | tee -a /etc/environment > /dev/null
-locale-gen en_US en_US.UTF-8
-dpkg-reconfigure locales
 
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
+
+apt-get autoremove -y
